696cd6f5da92add60de005c473c60e0aa236f762
John Murray <heyjohnmurray@gmail.com>
2012-10-09 00:41:24 -0400

    continue to restructure 'blog' folder

--- includes/header.php
+++ includes/header.php
@@ -13,24 +13,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 <![endif]-->
 
-<!-- Open Graph -->
-<meta property="og:title" content=""/>
-<meta property="og:description" content=""/>
-<meta property="og:type" content="website"/>
-<meta property="og:url" content=""/>
-<meta property="og:site_name" content=""/>
-<meta property="fb:app_id" content=""/>
-<meta property="og:image" content="images/fb_icon.png"/>
-<!-- Open Graph -->
-
 <meta name="description" content=""/>
 <meta name="keywords" content=""/>
 <link rel="canonical" href=""/>
 
-<link rel="shortcut icon" href="images/favicon.ico"/> 
-<link rel="apple-touch-icon" href="images/touch_icon.png" type="image/png"/>
-
-<link rel="stylesheet" href="fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen"/>
 <link rel="stylesheet" href="css/box_grid.css" type="text/css" media="all"/>
 <link rel="stylesheet" href="css/style_standards.css" type="text/css" media="screen"/>
 <link rel="stylesheet" href="css/style_main.css" type="text/css" media="screen"/>
