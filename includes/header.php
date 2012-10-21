c288e3b97b4150de6c5211c7eb70190db45d9ee3
John Murray <heyjohnmurray@gmail.com>
2012-10-21 19:16:00 -0400

    basic page template

--- includes/header.php
+++ includes/header.php
@@ -19,9 +19,26 @@
 
 <link rel="stylesheet" href="css/box_grid.css" type="text/css" media="all"/>
 <link rel="stylesheet" href="css/style_standards.css" type="text/css" media="screen"/>
-<link rel="stylesheet" href="css/style_main.css" type="text/css" media="screen"/>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,400' rel='stylesheet' type='text/css'>
 
 <script src="js/modernizr.js"></script>
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 
 </head>
+
+<!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
+<body class="<?= $page ?>">
+<div class="wrapper clearfix">
+	<header role="banner">
+		<div class="box-16">
+			<div class="row">
+				<div class="box-8"><img src="images/logo.jpg" width="370" height="80" alt="" class="logo" /></div>
+				<div class="box-8"></div>
+			</div>
+			<div class="row">
+				<? include_once("includes/_main_nav.php"); ?>
+			</div><!--/.row-->
+		</div><!--/.box-16-->
+	</header>
