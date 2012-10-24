d41d640f51ea6873bf3d2e5e2966322075e0658f
John Murray <heyjohnmurray@gmail.com>
2012-10-23 23:32:14 -0400

    don't care about this rollback

--- add-post.php
+++ add-post.php
@@ -1,39 +1,38 @@
 <?
-	ob_start();
-	session_start();
-	$errors = array();
-	$success = array();					
-	$_SESSION['errors'] = $errors;
-	$_SESSION['success'] = $success;
-	$page = "add-post-page";	
-	$title = "Add a Post";	
 	require_once('../_db_connect.php');
-	require_once('includes/header.php'); 	
+	$page = "add-post-page";	
+	$title = "Add a Post";
+	require_once('includes/header.php'); 
 ?>
 	<section id="content">
 		<div class="box-16">
 			<div class="row">
 				<div class="box-10 centered-content">
 					<h1>Add a Post</h1>
+<<<<<<< HEAD
 					<div class="errors" id="errors">
 						<script type="text/javascript">
 							$(function() {
 								<?		
 									if(isset($_SESSION['errors'])){?>
 										setTimeout(function(e){
+=======
+					<?									
+						session_start();	
+						//print_r($_SESSION['errors']); //use this if the form isn't validating right
+					?>
+					<p class="errors" id="errors">
+						<?															
+							if(!empty($_SESSION['errors'])){
+						?>
+								<script type="text/javascript">
+									$(function() {
+										setTimeout(function(){
+>>>>>>> parent of dff8e52... This code seesm wrong but ...
 											$("#errors").fadeIn(500);
-										}, 500)	
-								<?
-										foreach($_SESSION['errors'] as $error){
-											echo $error;
-										}
-									}
-									
-									if(isset($_SESSION['success'])){ 									
-								?>
-										setTimeout(function(e){
-											$("#errors").fadeIn(500).addClass("success").text("<?= $_SESSION['success'] ?>");
+											alert('Find out how to add class to highlight inputs, too!');											
 										}, 500)
+<<<<<<< HEAD
 								<?	
 									} 
 											
@@ -46,6 +45,35 @@
 					</div>
 					<form action="" method="post" name="add-post" class="admin-form add-post">						
 						<label for="post-name">Title</label>
+=======
+									//close jquery	
+									});								
+								</script>								
+						<?
+								foreach($_SESSION['errors'] as $error){
+									echo $error;
+								}
+								session_unset($_SESSION['errors']);
+							} 
+							
+							if(!empty($_SESSION['success'])){
+						?>
+							<script type="text/javascript">
+								$(function() {
+									setTimeout(function(){
+										$("#errors").fadeIn(500).addClass("success").text("<?= $_SESSION['success'] ?>");
+									}, 500)
+								//close jquery	
+								});								
+							</script>	
+						<?				
+								session_unset($_SESSION['success']);	
+							}
+						?>
+					</p>
+					<form action="add-post-post2.php" method="post" name="add-post" class="admin-form add-post">						
+						<label for="post-title">Title</label>
+>>>>>>> parent of dff8e52... This code seesm wrong but ...
 						<input type="text" name="post-title" value="" class="post-title" />
 						<label for="post-subhead">Subhead <span class="optional">(Optional)</span></label>
 						<input type="text" name="post-subhead" value="" class="post-subhead" />
--- index.php
+++ index.php
@@ -3,32 +3,21 @@
 	$page = "posts-page";	
 	$title = "List of posts";
 	require_once('includes/header.php'); 
-	
-	$query = "SELECT * FROM posts";
-	$result = mysqli_query($dbconnect,$query);	
 ?>
 	<section id="content">
 		<div class="box-16">
 			<div class="row">
 				<div class="box-10 two-third left-content">
 					<h1>Recent Posts</h1>
-					<?
-						if($result){
-							while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){												echo"<article>
-									<hgroup>
-										<h2>" . $row['post-title'] . "</h2>
-										<h3>" . $row['post-subhead'] . "</h3>					
-									</hgroup>
-									<time>Posted on" . $row['post-date'] . "</time>
-									<p>" . $row['post-content'] . "</p>
-									<p><a href=\"#\" class=\"read-more\">Read More</a></p>
-								</article>";				
-							}
-							mysqli_free_result($result);
-						} else {
-							echo "<p>" . mysqli_error($dbconnect) . "</p>";
-						}
-					?>						
+					<article>
+						<hgroup>
+							<h2>Post One Header</h2>							
+							<h3>Subhead</h3>							
+						</hgroup>
+							<time>Posted on December, 08, 2012</time>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<p><a href="#" class="read-more">Read More</a></p>
+					</article>					
 				</div><!-- close left content -->
 				<div class="box-5 third right-content">
 					<aside>
