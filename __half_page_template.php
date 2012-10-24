398034068afb76a004a0e2a62e44ac135dd90582
John Murray <heyjohnmurray@gmail.com>
2012-10-24 02:10:16 -0400

    making final template edits before I branch this

---
+++ __half_page_template.php
@@ -0,0 +1,20 @@
+<?
+	require_once('../_db_connect.php');
+	$page = "";//body tag class
+	$title = "";//title tag text
+	require_once('includes/header.php'); 
+?>
+	<section id="content">
+		<div class="box-16">
+			<div class="row">
+				<div class="box-10 centered-content">
+					<h1>XXTITLEXX</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>						
+				</div><!-- close centered content -->
+			</div><!-- close row -->
+		</div><!--/.box-16-->	
+	</section>
+<? require_once('includes/footer.php'); ?>
+</div>
+</body>
+</html>
