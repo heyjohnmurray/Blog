dff8e52eef6958cb773cd5ff01347d3c5c4a460b
John Murray <heyjohnmurray@gmail.com>
2012-10-23 23:23:30 -0400

    This code seesm wrong but ...
    i'm keeping it anyway just in case there's stuff i want.

--- add-post-post2.php
+++ add-post-post2.php
@@ -1,26 +1,40 @@
 <?	
-	if($_SERVER['REQUEST_METHOD'] ==  "POST"){
-		session_start();					
-		if(!empty($_POST['post-title']) && !empty($_POST['post-content'])){
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		session_start();
+		$errors = array();
+		$success = array();		
+		$_SESSION['errors'] = $errors;
+		$_SESSION['success'] = $success;			
+		
+		if(empty($_POST['post-title'])){		
+		    $errors[] = "Please enter a title for this post.";
+		} else{
+			$post_title = strip_tags($_POST['post_title']);
+		}
+		
+		if(empty($_POST['post-content'])){
+		    $errors[] = "Please enter content for this post.";
+		} else{
+			$post_content = strip_tags($_POST['post_content']);
+		}
+		
+		$post_subhead = strip_tags($_POST['post_subhead']);     								
+				
+		if(empty($errors)){	//everything is ok
+					
+			$query = 'INSERT INTO posts (post-title, post-subhead, post-content) values ($post_title, $post_subhead, $post_content)'; 
 			
-			//run query
-			$_SESSION['success'] = $success;
-			$success = "Your post has been published!";
-			
-		} else{			
+			$result = mysqli_query($dbconnect, $query);			
 								
-			
-			$_SESSION['errors'] = $errors;			
-			$errors = array();
-			
-			if(empty($_POST['post-title'])){
-				$errors[] = "Please enter a title for this post. <br />";
+			if($result){
+				$success[] = "Your post has been published!";
+			} else{
+				$errors[] = "Error" . mysqli_error($dbconnect);
 			}
-			
-			if(empty($_POST['post-content'])){
-				$errors[] = "Please enter content for this post.";
-			}					
-		}		
+												
+		}	
+		
+		//mysqli_close($dbconnect); 	
 
 		header("Location: add-post.php");
 		exit();
