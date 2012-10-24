b920ce296ee3bbf5805e899788c5da2862693949
John Murray <heyjohnmurray@gmail.com>
2012-10-24 01:18:44 -0400

    fucccccccck

--- index.php
+++ index.php
@@ -1,72 +1,25 @@
-<? require_once('includes/header.php'); ?>
-<<<<<<< HEAD
-
 <?
 	require_once('../_db_connect.php');
 	$page = "posts-page";	
 	$title = "List of posts";
 	require_once('includes/header.php'); 
-?>
-
-<body>
-
-	<header role="banner">
-		<div class="container">
-			<div class="row">
-				<div class="box-16">
-					<nav role="navigation">
-						<ul>
-							<li>nav</li>
-						</ul>
-					</nav>
-				</div><!--/.box-16-->
-			</div><!--/.row-->
-		</div><!--/.container-->
-	</header>
-	
-	<section id="content">
-		<div class="container">
-			<div class="row">
-				<div class="box-16">
-					content
-				</div><!--/.box-16-->
-			</div><!--/.row-->
-		</div><!--/.container-->
-	</section>
-
-=======
->>>>>>> parent of 8ab04ba... update page templates
+?>	
 	<section id="content">
 		<div class="box-16">
 			<div class="row">
-				<div class="box-10 two-third left-content">
+				<div class="box-10 left-content">
 					<h1>Recent Posts</h1>
 					<article>
 						<hgroup>
 							<h2>Post One Header</h2>							
 							<h3>Subhead</h3>
-<<<<<<< HEAD
-							<time>Posted on December, 08, 2012</time>
 						</hgroup>
+						<time>Posted on December, 08, 2012</time>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>						
 					</article>					
-				</div>
-
-							<h3>Subhead</h3>							
-						</hgroup>
-=======
->>>>>>> parent of 8ab04ba... update page templates
-							<time>Posted on December, 08, 2012</time>
-						</hgroup>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>						
-					</article>					
-<<<<<<< HEAD
 				</div><!-- close left content -->
 
-=======
-				</div>
->>>>>>> parent of 8ab04ba... update page templates
-				<div class="box-5 third right-content">
+				<div class="box-5 right-content">
 					<aside>
 						<h2>About Us</h2>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
